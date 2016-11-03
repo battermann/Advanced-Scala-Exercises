a51b3edfa5c98d9a43dba06e20c42f9bc0cbd772
Leif Battermann <leifbattermann@gmail.com>
2016-11-03 14:42:19 +0100

    case study pygmy haddop continued

--- src/test/scala/com/advancedscala/casestudy08/PygmyHadoop.scala
+++ src/test/scala/com/advancedscala/casestudy08/PygmyHadoop.scala
@@ -1,11 +1,13 @@
 package com.advancedscala.casestudy08
 
-import cats.Monoid
+import cats.data.Xor
+import cats.{Id, Monad, Monoid}
 import cats.implicits._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import org.scalatest.FunSuite
 
+import scala.language.higherKinds
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
 
@@ -27,8 +29,43 @@ class PygmyHadoop extends FunSuite {
     Future.sequence(foldedPartials).map(x => foldMap(x.toIterable)(identity))
   }
 
+  def foldMapM[A, M[_]: Monad, B: Monoid](iter: Iterable[A])(f: A => M[B] = (a: A) => a.pure[Id]): M[B] =
+    iter.foldLeft(Monad[M].pure(Monoid[B].empty))((acc, el) => for {
+      x1 <- acc
+      x2 <- f(el)
+    } yield x1 |+| x2)
+
+  def foldMap2[A, B: Monoid](values: Iterable[A])(func: A => B = (a: A) => a): B =
+    foldMapM(values)(x => func(x).pure[Id])
+
+  type ErrorOr[A] = String Xor A
+
+  test("foldMapM Xor") {
+    val seq = Seq("1", "2", "3")
+
+    assert(foldMapM[String, ErrorOr, Int](seq)(x => Xor.catchOnly[NumberFormatException](x.toInt).leftMap(_ => s"Cannot parse input string: $x.")) == 6.right[String])
+
+    val seqInValid = Seq("1", "abc", "3")
+
+    assert(foldMapM[String, ErrorOr, Int](seqInValid)(x => Xor.catchOnly[NumberFormatException](x.toInt).leftMap(_ => s"Cannot parse input string: $x.")) == "Cannot parse input string: abc.".left[Int])
+  }
+
   test("foldMap") {
-    assert(foldMap(List(1, 2, 3))(identity) == 6)
+    assert(foldMap(List(1, 2, 3))() == 6)
+  }
+
+  test("foldMapP") {
     assert(Await.result(foldMapP(1 to 1000)(identity), Duration.Inf) ==  500500)
+  }
+
+  test("foldMapM") {
+    val seq = List(1, 2, 3)
+    assert(foldMapM(seq)(a => Option(a)) == Some(6))
+
+    assert(foldMapM(seq)() == 6)
+  }
+
+  test("foldMap2") {
+    assert(foldMap2(List(1, 2, 3))() == 6)
   }
 }
